d45c24986ce4696583855f16ebd65fbe6261efaa
marycodes2 <mbeliveau92@gmail.com>
2018-09-19 09:57:47 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+class Book
+  attr_accessor :page_count
+  attr_accessor :genre
 
+  def initialize(title)
+    @title = title
+  end
 
+  def title
+    @title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
